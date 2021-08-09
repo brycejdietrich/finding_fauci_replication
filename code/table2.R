e086b055da25be47198c5b5c8d0a7dc1fa91b817
Bryce J. Dietrich <brycedietrich@gmail.com>
2021-08-09 12:13:34 -0400

    Create table2.R

---
+++ code/table2.R
@@ -0,0 +1,19 @@
+setwd('/Users/brycedietrich/Research_Group Dropbox/bryce dietrich/dietrich_ko_replication/')
+
+require(MASS)
+require(stargazer)
+
+results<-read.csv('data/final_celebrity_results.csv',as.is=T)
+
+#make fox baseline
+results$network2<-factor(results$network,levels=c('fox','cnn','msnbc'))
+
+#set week 9 to zero so the intercept is meaningful
+results$week2<-results$week-9
+
+#negative binomial regressions with offsets
+mod1<-glm.nb(fauci~network2+offset(log(cc)),data=results)
+mod2<-glm.nb(fauci~network2*week2+offset(log(cc)),data=results)
+
+#export table
+stargazer(mod1,mod2,intercept.bottom = F,title='Table 2: Dr. Anthony Fauci is Less Likely to Appear on Fox News When the Network is Discussing Covid-19',dep.var.labels=c('Fauci Appearances'),covariate.labels=c('Constant','CNN','MSNBC','Week','CNN X Week','MSNBC X Week'),type='html',out='output/table2.html')
